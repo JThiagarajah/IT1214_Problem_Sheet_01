3038dead7d0e231268d9825f16a174891de566ad
JThiagarajah <joshuathiagarajah@gmail.com>
2025-05-17 10:17:38 +0000

    Created 02.java

--- 01.java
+++ 01.java
@@ -1,3 +1,7 @@
 class Student{
-
+public static void main(String[] args){
+System.out.println();
+System.out.println("Hello Joshua");
+System.out.println("Hello again from the second line of code");
+}
 }
