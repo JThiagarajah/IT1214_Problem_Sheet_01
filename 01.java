6ad158cd1ffe6c2a5573d7642bbf4c84f6415fbd
JThiagarajah <joshuathiagarajah@gmail.com>
2025-05-18 21:21:38 +0530

    Delete 01.java

--- 01.java
+++
@@ -1,7 +0,0 @@
-class Student{
-public static void main(String[] args){
-System.out.println();
-System.out.println("Hello Joshua");
-System.out.println("Hello again from the second line of code");
-}
-}
